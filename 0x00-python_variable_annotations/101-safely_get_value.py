9bf9b278286d86df575afd15420b04e78f10cac1
SteveCmd <murimi101@gmail.com>
2024-10-11 14:17:09 +0300

    11. More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,12 +6,11 @@ Module for a type-annotated function safely_get_value
 from typing import Any, Mapping, TypeVar, Union
 
 T = TypeVar('T')
+Res = Union[Any, T]
+Def = Union[T, None]
 
 
-def safely_get_value(
-        dct: Mapping[Any, Any],
-        key: Any, default: Union[T, None] = None
-        ) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
     """
     Returns the value for the given key if it exists in the dictionary,
     otherwise returns the default value.
