3d5ec2ff5381c1f7d3028c65a7fde62c1cae972f
SteveCmd <murimi101@gmail.com>
2024-10-15 20:24:36 +0300

    Refactor async generator to use imported functions
    The code changes in `0-async_generator.py` refactor the `async_generator` function to use imported functions `sleep` from `asyncio` and `uniform` from `random` instead of directly importing `asyncio` and `random`. This improves code readability and maintainability.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ async generator """
 
-import asyncio
-import random
+from asyncio import sleep
+from random import uniform
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
     for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        await sleep(1)
+        yield uniform(0, 10)
