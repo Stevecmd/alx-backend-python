c0c78ffba3c1fad6065921edfdf08db9a43c0762
SteveCmd <murimi101@gmail.com>
2024-10-15 20:30:37 +0300

    added comments

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
+    """ async generator """
     for _ in range(10):
         await sleep(1)
         yield uniform(0, 10)
