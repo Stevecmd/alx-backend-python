cf629d64290ad01931dcdabc0a847c43ab917c81
SteveCmd <murimi101@gmail.com>
2024-11-02 15:02:27 +0300

    3. Parameterize and patch Add unit tests for memoize function in test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
-from utils import get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -46,6 +45,31 @@ class TestGetJson(unittest.TestCase):
         result = get_json(test_url)
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """TestMemoize class to test memoize function"""
+
+    def test_memoize(self):
+        """Test memoize function"""
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+            with patch.object(
+                    TestClass,
+                    'a_method',
+                    return_value=42
+            ) as mock_method:
+                test_instance = TestClass()
+                self.assertEqual(test_instance.a_property, 42)
+                self.assertEqual(test_instance.a_property, 42)
+                mock_method.assert_called_once()
 
 
 if __name__ == '__main__':
